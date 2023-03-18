a83e367f8325261e14a6f91d92b07d5b0f872497
Nobody1421 <sebastianarango0305@gmail.com>
2023-03-18 18:59:30 -0400

    gets area of square

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int length = sc.nextInt();
+        int length = 5;
         System.out.println(Square(length));
     }
     public static int Square(int length){
